dc75486e5e825c65cbd191efca68bcc7982a7d3e
Richard <duddydad490@gmail.com>
2025-01-25 00:48:30 +0700

    fix:RentalPage_Error

--- src/app/rentals/page.tsx
+++ src/app/rentals/page.tsx
@@ -1,20 +1,11 @@
-"use client";
 
-import FormContainer from "@/components/form/FormContainter";
-import { Input } from "@/components/ui/input";
-import { getUserName } from "@/Utils/actions";
+
+
 
 export default function Rentals() {
   return (
-    <FormContainer action={getUserName}>
-      <Input name="userName" placeholder="Enter your username" />
-
-      <button
-        type="submit"
-        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Submit
-      </button>
-    </FormContainer>
+    <div>
+      Rental
+    </div>
   );
 }

771da149ca4a9ead5980ddf4295c2074f0252cea
Richard <duddydad490@gmail.com>
2025-02-07 18:53:08 +0700

    fix:middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/property(.*)"]);
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
   const isAdminUser = userId === process.env.ADMIN_USER_ID;
-  
+
   if (isAdminRoute(req) && !isAdminUser) {
     return NextResponse.redirect(new URL("/", req.url));
   }
